40b464043d087786b098326a5977b22620df3aa8
PrisonShift3r <dimaprisen24@gmail.com>
2024-12-24 23:29:15 +0300

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -10,14 +10,14 @@ from .forms import PostForm, ProfileEditForm, CommentForm
 
 def get_posts(post_objects):
     """Посты из БД."""
-    return post_objects.filter(
+    return post_objects.filter( #1.13 Фильтрация записей по опубликованности
         pub_date__lte=timezone.now(),
         is_published=True,
         category__is_published=True
-    ).annotate(comment_count=Count('comments'))
+    ).annotate(comment_count=Count('comments')) #1.8 количество комментариев для всех наборов постов; 1.9 метод annotate для добавления количества комментариев
 
 
-def get_paginator(request, items, num=10):
+def get_paginator(request, items, num=10): #1.11 Функция обрабатывает создание объекта пагинации
     """Создает объект пагинации."""
     paginator = Paginator(items, num)
     num_pages = request.GET.get('page')
@@ -27,17 +27,17 @@ def get_paginator(request, items, num=10):
 def index(request):
     """Главная страница."""
     template = 'blog/index.html'
-    post_list = get_posts(Post.objects).order_by('-pub_date')
+    post_list = get_posts(Post.objects).order_by('-pub_date') #1.10 Сортировка после annotate
     page_obj = get_paginator(request, post_list)
     context = {'page_obj': page_obj}
     return render(request, template, context)
 
 
-def post_detail(request, post_id):
+def post_detail(request, post_id): #1.16 Проверка автора поста
     """Полное описание выбранной записи."""
     template = 'blog/detail.html'
-    posts = get_object_or_404(Post, id=post_id)
-    if request.user != posts.author:
+    posts = get_object_or_404(Post, id=post_id) #Пост извлекается без фильтрации
+    if request.user != posts.author: #Если пользователь не автор, выполняется повторное извлечение из опубликованных постов
         posts = get_object_or_404(get_posts(Post.objects), id=post_id)
     comments = posts.comments.order_by('created_at')
     form = CommentForm()
@@ -56,7 +56,7 @@ def category_posts(request, category_slug):
     return render(request, template, context)
 
 
-@login_required
+@login_required #1.14 Декоратор для функций
 def create_post(request):
     """Создает новую запись."""
     template = 'blog/create.html'
@@ -66,14 +66,14 @@ def create_post(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('blog:profile', request.user)
+            return redirect('blog:profile', request.user) #1.6 URL должны вычисляется через имя маршрута; без reverce
     else:
         form = PostForm()
     context = {'form': form}
     return render(request, template, context)
 
 
-def profile(request, username):
+def profile(request, username): #1.12 Видимость постов автора
     """Возвращает профиль пользователя."""
     template = 'blog/profile.html'
     user = get_object_or_404(User, username=username)
@@ -95,7 +95,7 @@ def edit_profile(request):
         form = ProfileEditForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('blog:profile', request.user)
+            return redirect('blog:profile', request.user) #1.6
     else:
         form = ProfileEditForm(instance=request.user)
     context = {'form': form}
@@ -106,9 +106,9 @@ def edit_profile(request):
 def edit_post(request, post_id):
     """Редактирует запись блога."""
     template = 'blog/create.html'
-    post = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, id=post_id) #1.7 Извлечение объектов через get_object_or_404
     if request.user != post.author:
-        return redirect('blog:post_detail', post_id)
+        return redirect('blog:post_detail', post_id) #1.6
     if request.method == "POST":
         form = PostForm(
             request.POST, files=request.FILES or None, instance=post)
@@ -125,13 +125,13 @@ def edit_post(request, post_id):
 def delete_post(request, post_id):
     """Удаляет запись блога."""
     template = 'blog/create.html'
-    post = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, id=post_id) #1.7
     if request.user != post.author:
-        return redirect('blog:post_detail', post_id)
+        return redirect('blog:post_detail', post_id) #1.6
     if request.method == 'POST':
         form = PostForm(request.POST or None, instance=post)
         post.delete()
-        return redirect('blog:index')
+        return redirect('blog:index') #6
     else:
         form = PostForm(instance=post)
     context = {'form': form}
@@ -141,21 +141,21 @@ def delete_post(request, post_id):
 @login_required
 def add_comment(request, post_id):
     """Добавляет комментарий к записи."""
-    post = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, id=post_id) #7
     form = CommentForm(request.POST or None)
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
         comment.author = request.user
         comment.save()
-    return redirect('blog:post_detail', post_id)
+    return redirect('blog:post_detail', post_id) #1.6
 
 
 @login_required
 def edit_comment(request, post_id, comment_id):
     """Редактирует комментарий."""
     template = 'blog/comment.html'
-    comment = get_object_or_404(Comment, id=comment_id)
+    comment = get_object_or_404(Comment, id=comment_id) #1.7
     if request.user != comment.author:
         return redirect('blog:post_detail', post_id)
     if request.method == "POST":
@@ -173,7 +173,7 @@ def edit_comment(request, post_id, comment_id):
 def delete_comment(request, post_id, comment_id):
     """Удаляет комментарий."""
     template = 'blog/comment.html'
-    comment = get_object_or_404(Comment, id=comment_id)
+    comment = get_object_or_404(Comment, id=comment_id) #1.7
     if request.user != comment.author:
         return redirect('blog:post_detail', post_id)
     if request.method == "POST":
