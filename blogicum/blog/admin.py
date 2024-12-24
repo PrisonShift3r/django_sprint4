fecd57b1c9af1e7ee55f0e2f6c96b3fcd2dc532c
PrisonShift3r <dimaprisen24@gmail.com>
2024-12-24 23:51:11 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Location, Category, Post, Comment
+from .models import Location, Category, Post, Comment #1.1 Модели Category, Location, Post, Comment зарегистрированы в админке.
 
 admin.site.register(Location)
 admin.site.register(Category)
